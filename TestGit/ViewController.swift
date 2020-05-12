0747c892279a654b8135e01ab5be44d9aaafc143
Habib Akbar <habibakbar360@gmail.com>
2020-05-12 19:11:25 +0700

    Initial commit

---
+++ TestGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestGit
+//
+//  Created by M Habib Ali Akbar on 12/05/20.
+//  Copyright © 2020 Group 16 MC2 2020. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
